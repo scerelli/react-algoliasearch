e125b5a77b44130eb79df6b2860b43aac3141e8c
Stefano Cerelli <info@stefanocerelli.com>
2016-06-22 16:33:16 +0200

    Change dist filename, add gh url to package

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -26,8 +26,8 @@ const config = {
     demo: path.join(ROOT_PATH, 'demo'),
     tests: path.join(ROOT_PATH, 'tests')
   },
-  filename: 'boilerplate',
-  library: 'Boilerplate'
+  filename: 'react-algoliasearch',
+  library: 'ReactAlgoliasearch'
 };
 const CSS_PATHS = [
   config.paths.demo,
